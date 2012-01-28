ef81a4e5394a136ff92cd306f4b48d8b500cf5e8
MarsFlyer <paul_t_allen@yahoo.com>
2012-01-28 16:21:40 +0000

    Config data for Pachube.

---
+++ GasLogNanode/Config.h
@@ -0,0 +1,15 @@
+#ifndef GASLOG_CONFIG_H_
+#define GASLOG_CONFIG_H_
+
+// Pachube:
+#define PACHUBE_VHOST "api.pachube.com"
+#define PACHUBEAPIKEY "X-PachubeApiKey: xxx" 
+#define PACHUBEAPIURL "/v2/feeds/999.csv"
+
+// RFM12:
+#define MYNODE 2            // node ID 30 reserved for base station
+#define freq RF12_868MHZ     // frequency
+#define group 5            // network group 
+
+#endif // PACHUBE_CONFIG_H_
+
